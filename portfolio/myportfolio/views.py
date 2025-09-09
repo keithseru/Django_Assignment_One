a639f4527e499f57d2d7e69ddbd35e98923a44ee
Keith James Seruyange <keithseruyange@gmail.com>
2025-09-09 23:16:21 +0300

    Made changes to the template pages

--- portfolio/myportfolio/views.py
+++ portfolio/myportfolio/views.py
@@ -11,4 +11,8 @@ def educationPage(request):
     return render(request, "education.html")
 
 def contactPage(request):
+    context = {}
+    if request.method == 'POST':
+        context["thank_you"] = "Thank you for reaching out! I will get back to you soon."
+        return render(request, "contact.html", context)
     return render(request, "contact.html")
