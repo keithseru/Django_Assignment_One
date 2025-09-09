ad00dee6c65b86ab267451fdb4fdd845161deac7
Keith James Seruyange <keithseruyange@gmail.com>
2025-09-09 20:22:34 +0300

    Made initail changes to html templates

--- portfolio/myportfolio/views.py
+++ portfolio/myportfolio/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def aboutMe(request):
-    return render(request)
+    return render(request, "about_me.html")
 
 def educationPage(request):
-    return render(request)
+    return render(request, "education.html")
 
 def contactPage(request):
-    return render(request)
+    return render(request, "contact.html")
